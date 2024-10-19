a739d3f4e3985f9db26ea890c9a469d6237bc217
WavyWare <jflortech@gmail.com>
2024-10-19 16:05:33 +0200

    Added Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from './components/Home.tsx'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
