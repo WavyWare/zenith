ae7581ff74486133dcfc9f6b2610b28e85eb254b
kapi7676 <kacperstachula768@gmail.com>
2024-11-25 09:18:24 +0100

    Add padding for mobile view

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,9 +5,11 @@ function Navigation() {
     return (
         <ThemeProvider defaultTheme={"system"} storageKey={"vite-ui-theme"}>
             <div className={"w-screen dark:border-violet-500 mx-auto py-5 border-b"}>
-                <div className={"flex justify-around mx-auto"}>
-                    <span className={"font-bold text-violet-800 dark:text-violet-500 font-serif text-3xl"}>zenith 🌑</span>
-                    <ModeToggle/>
+                <div className={"flex md:justify-around justify-between mx-auto"}>
+                    <span className={"font-bold text-violet-800 dark:text-violet-500 font-serif text-3xl px-6"}>zenith 🌑</span>
+                    <div className="px-6">
+                        <ModeToggle />
+                    </div>
                 </div>
             </div>
         </ThemeProvider>
